c184df31e7f13f97d08eb3449294ed088f8ab49c
Colby Boettcher <cboettcher16590@alumni.focuscollege.com>
2020-10-06 13:58:05 -0700

    Added get-house-materials command

---
+++ src/commands/getHouseMaterials.ts
@@ -0,0 +1,34 @@
+import { Arguments, Argv } from "yargs";
+
+export function getHouseMaterials( yargs:Argv ):void {
+
+    yargs.command(
+        // Creates a new yargs command
+        "get-house-materials",
+
+        //Describes the new yargs command
+        "Gets the materials needed for a specific house that has been saved",
+
+        //Defines the parameters we need for the command
+        {
+            name: {
+                type: "string",
+                alias: "n",
+                description: "The name of the saved house"
+            },
+        },
+
+        function (
+            args: Arguments<{
+                name: string;
+                n: string;
+            }>
+        ) {
+        //Here we can insert our code for the command function   
+        
+            console.log("The parameter passed in is Name: " + args.name );
+        }
+
+    );
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import yargs = require('yargs');
 import { calcHouseMaterials } from './commands/calcHouseMaterials';
+import { getHouseMaterials } from './commands/getHouseMaterials';
 
 calcHouseMaterials( yargs );
 
-
+getHouseMaterials( yargs );
 
 //tells yargs to include the --help flag
 yargs.help();
